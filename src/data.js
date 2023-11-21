d30a0752d7ac8c41ed36c36a49e9e330ef64316e
cyberserk7 <deynilabjo@gmail.com>
2023-11-21 21:18:36 +0530

    ..

--- src/data.js
+++ src/data.js
@@ -8,7 +8,7 @@ export const cards = [
 	{
 	  id: 2,
 	  title: "Logo Design",
-	  desc: "Build yor brand",
+	  desc: "Build your brand",
 	  img: "https://images.pexels.com/photos/11295165/pexels-photo-11295165.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
 	},
 	{
