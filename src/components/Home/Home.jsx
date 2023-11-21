bc8c7b74a4ea1c527ed883c6d7979f956d6bc7f6
cyberserk7 <deynilabjo@gmail.com>
2023-11-22 03:45:04 +0530

    commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , { useState} from 'react'
 import Featured from "../Featured/Featured"
 import TrustedBy from '../TrustedBy/TrustedBy'
 import Slide from '../Slide/Slide'
@@ -14,6 +14,16 @@ import { projects } from '../../data'
 import "./Home.scss"
 
 function Home() {
+  const [hover1, setHover1] = useState(false);
+  const [hover2, setHover2] = useState(false);
+  const [hover3, setHover3] = useState(false);
+  const [hover4, setHover4] = useState(false);
+  const [hover5, setHover5] = useState(false);
+  const [hover6, setHover6] = useState(false);
+  const [hover7, setHover7] = useState(false);
+  const [hover8, setHover8] = useState(false);
+  const [hover9, setHover9] = useState(false);
+  const [hover10, setHover10] = useState(false);
   return (
 	<div className='home'>
     <Featured />
@@ -24,85 +34,96 @@ function Home() {
       <div className="container">
         <h1>You need it, we've got it</h1>
         <div className="div-one">
-          <div className="individual-icons">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M9.53 16.122a3 3 0 00-5.78 1.128 2.25 2.25 0 01-2.4 2.245 4.5 4.5 0 008.4-2.245c0-.399-.078-.78-.22-1.128zm0 0a15.998 15.998 0 003.388-1.62m-5.043-.025a15.994 15.994 0 011.622-3.395m3.42 3.42a15.995 15.995 0 004.764-4.648l3.876-5.814a1.151 1.151 0 00-1.597-1.597L14.146 6.32a15.996 15.996 0 00-4.649 4.763m3.42 3.42a6.776 6.776 0 00-3.42-3.42" />
-            </svg>
-            <hr />
-            <h2>Graphic & Design</h2>
-          </div>
+          <Link to="/gigs" className='link'>
+            <div className="individual-icons" onMouseEnter={() => setHover1(true)} onMouseLeave={() => setHover1(false)}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9.53 16.122a3 3 0 00-5.78 1.128 2.25 2.25 0 01-2.4 2.245 4.5 4.5 0 008.4-2.245c0-.399-.078-.78-.22-1.128zm0 0a15.998 15.998 0 003.388-1.62m-5.043-.025a15.994 15.994 0 011.622-3.395m3.42 3.42a15.995 15.995 0 004.764-4.648l3.876-5.814a1.151 1.151 0 00-1.597-1.597L14.146 6.32a15.996 15.996 0 00-4.649 4.763m3.42 3.42a6.776 6.776 0 00-3.42-3.42" />
+              </svg>
+              {!hover1 ? <hr className='normal' /> : <hr className='active'/>}
+              {/* <hr /> */}
+              <h2>Graphic & Design</h2>
+            </div>
+          </Link>
 
-          <div className="individual-icons">
+          <div className="individual-icons" onMouseEnter={() => setHover2(true)} onMouseLeave={() => setHover2(false)}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25" />
             </svg>
-            <hr />
+            {!hover2 ? <hr className='normal' /> : <hr className='active'/>}
             <h2>Digital Marketing</h2>
           </div>
 
-          <div className="individual-icons">
+          <div className="individual-icons"
+          onMouseEnter={() => setHover3(true)} onMouseLeave={() => setHover3(false)}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
             </svg>
-            <hr />
+            {!hover3 ? <hr className='normal' /> : <hr className='active'/>}
             <h2>Writing & Translation</h2>
           </div>
 
-          <div className="individual-icons">
+          <div className="individual-icons"
+          onMouseEnter={() => setHover4(true)} onMouseLeave={() => setHover4(false)}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
             </svg>
 
-            <hr />
+           {!hover4 ? <hr className='normal' /> : <hr className='active'/>}
             <h2>Video & Animation</h2>
           </div>
 
-          <div className="individual-icons">
+          <div className="individual-icons"
+          onMouseEnter={() => setHover5(true)} onMouseLeave={() => setHover5(false)}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z" />
             </svg>
-            <hr />
+            {!hover5 ? <hr className='normal' /> : <hr className='active'/>}
             <h2>Audio & Music</h2>
           </div>
 
         </div>
         <div className="div-two">
-          <div className="individual-icons">
+          <div className="individual-icons"
+          onMouseEnter={() => setHover6(true)} onMouseLeave={() => setHover6(false)}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
             </svg>
-            <hr />
+            {!hover6 ? <hr className='normal' /> : <hr className='active'/>}
             <h2>Programming & Tech</h2>
           </div>
 
-          <div className="individual-icons">
+          <div className="individual-icons"
+          onMouseEnter={() => setHover7(true)} onMouseLeave={() => setHover7(false)}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z" />
             </svg>
-            <hr />
+            {!hover7 ? <hr className='normal' /> : <hr className='active'/>}
             <h2>Business</h2>
           </div>
 
-          <div className="individual-icons">
+          <div className="individual-icons"
+          onMouseEnter={() => setHover8(true)} onMouseLeave={() => setHover8(false)}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
             </svg>
 
-            <hr />
+            {!hover8 ? <hr className='normal' /> : <hr className='active'/>}
             <h2>Lifestyle</h2>
           </div>
 
-          <div className="individual-icons">
+          <div className="individual-icons"
+          onMouseEnter={() => setHover9(true)} onMouseLeave={() => setHover9(false)}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
             </svg>
 
 
-            <hr />
+            {!hover9 ? <hr className='normal' /> : <hr className='active'/>}
             <h2>Data</h2>
           </div>
 
-          <div className="individual-icons">
+          <div className="individual-icons"
+          onMouseEnter={() => setHover10(true)} onMouseLeave={() => setHover10(false)}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
   <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
@@ -110,7 +131,7 @@ function Home() {
 
 
 
-            <hr />
+{!hover10 ? <hr className='normal' /> : <hr className='active'/>}
             <h2>Photography</h2>
           </div>
 
